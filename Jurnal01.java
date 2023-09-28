c40752ffc19530d13e0a74f82ec75d4a37a0da23
gfadiyahhr <gfadiyahhr@gmail.com>
2023-09-28 19:34:27 +0700

    Finish Jurnal01 commit

--- Jurnal01.java
+++ Jurnal01.java
@@ -1,5 +1,8 @@
 public class Jurnal01 {
     public static void main(String[] args) {
-        
+        System.out.println("Nama: Ghina Fadiyah Hr");
+        System.out.println("NIM: 607062300001");
+        System.out.println("Kelas: D3I-47-01");
     }
+
 }
